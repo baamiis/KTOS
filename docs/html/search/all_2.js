4a0046cfe24f5128d654901c40c8bc8d24e811a3
baamiis <khamdou@icloud.com>
2026-04-19 13:31:51 +0100

    docs: fix tab bars dark bg with !important overrides, fix search text

--- docs/html/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['code_5fof_5fconduct_2emd_2',['CODE_OF_CONDUCT.md',['../CODE__OF__CONDUCT_8md.html',1,'']]],
-  ['contributing_2emd_3',['CONTRIBUTING.md',['../CONTRIBUTING_8md.html',1,'']]],
-  ['contributing_20to_20ktos_4',['Contributing to KTOS',['../md_CODE_OF_CONDUCT.html',1,'']]],
-  ['contributing_20to_20ktos_5',['Contributing to KTOS',['../md_CONTRIBUTING.html',1,'']]]
-];
--- docs/html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['g_5flasttaskreturnvalue_16',['g_LastTaskReturnValue',['../ktos_8c.html#ad41da386e0487d9ef764ca15def81b71',1,'ktos.c']]]
-];
